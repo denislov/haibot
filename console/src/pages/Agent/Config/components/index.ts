e04b0ddd871fd7b508fb417e67f74b779aacfc96
denislov <2864326614@qq.com>
2026-03-20 14:09:24 +0800

    rename copaw to haibot

--- console/src/pages/Agent/Config/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { SliderWithValue } from "./SliderWithValue";
-export { PageHeader } from "./PageHeader";
-export { ReactAgentCard } from "./ReactAgentCard";
-export { ContextManagementCard } from "./ContextManagementCard";
