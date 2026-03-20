d01692f63c44c079be01a636ef3f14e607b4e9a9
denislov <2864326614@qq.com>
2026-03-20 14:26:57 +0800

    build(frontend): update build output directory path

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -26,7 +26,7 @@ export default defineConfig(({ mode }) => {
       },
     },
     build: {
-      outDir: "../backend/haibot/console",
+      outDir: "../src/haibot/console",
       emptyOutDir: true,
       rollupOptions: {
         output: {
